7046fac548fcce2e50c0b4211e1346e44de8ba01
Ameer Aqel <ameer_agel@hotmail.com>
2022-11-05 13:10:21 +0200

    add userSelected Field

--- app/src/main/java/com/washcar/app/models/CategoryModel.kt
+++ app/src/main/java/com/washcar/app/models/CategoryModel.kt
@@ -1,10 +1,15 @@
 package com.washcar.app.models
 
+import com.google.firebase.firestore.Exclude
+
 
 data class CategoryModel(var id: String? = null, var name: String? = "") {
 
     var price: Double? = 0.0
     var selected: Boolean? = false
+
+    @Exclude
+    var userSelected: Boolean? = false
 
     override fun toString(): String {
         return "$id - $name - $price $selected"
