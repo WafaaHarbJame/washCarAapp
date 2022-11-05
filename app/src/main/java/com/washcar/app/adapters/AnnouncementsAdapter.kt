2728e18ccdbf3c65143a5a3b6fa961051cc36da3
WafaaHarbJame <49291280+WafaaHarbJame@users.noreply.github.com>
2022-11-05 21:28:48 +0200

    add app

--- app/src/main/java/com/washcar/app/adapters/AnnouncementsAdapter.kt
+++ app/src/main/java/com/washcar/app/adapters/AnnouncementsAdapter.kt
@@ -1,12 +1,18 @@
 package com.washcar.app.adapters
 
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.washcar.app.R
+import com.washcar.app.activities.ProfileActivity
+import com.washcar.app.activities.RequestCarActivity
+import com.washcar.app.classes.Constants
 import com.washcar.app.databinding.RowCarAnnouceBinding
+import com.washcar.app.databinding.RowCarWashBinding
 import com.washcar.app.models.MemberModel
 
 
@@ -23,16 +29,7 @@ class AnnouncementsAdapter(
     override fun onBindViewHolder(holder: Holder, position: Int) {
         val memberModel = list?.get(position)
 
-        holder.binding.tvName.text = memberModel?.fullName
-        holder.binding.tvAddress.text = memberModel?.address
-        holder.binding.tvDesc.text = memberModel?.description
-        holder.binding.tvTime.text = memberModel?.startTime?.plus(" | ${memberModel.endTime}")
-
-        Glide.with(context)
-            .asBitmap()
-            .load(memberModel?.photoUrl)
-            .placeholder(R.drawable.error_logo)
-            .into(holder.binding.ivUser)
+       holder.bind(memberModel)
     }
 
 
@@ -41,7 +38,44 @@ class AnnouncementsAdapter(
     }
 
 
-    inner class Holder(var binding: RowCarAnnouceBinding) : RecyclerView.ViewHolder(
-        binding.root
-    )
-}
+    inner class Holder(var binding: RowCarAnnouceBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+
+        fun bind(memberModel: MemberModel?) {
+            binding.tvName.text = memberModel?.fullName
+            binding.tvAddress.text = memberModel?.address
+            binding.tvDesc.text = memberModel?.description
+            binding.tvTime.text = memberModel?.startTime?.plus(" | ${memberModel.endTime}")
+
+            Glide.with(context)
+                .asBitmap()
+                .load(memberModel?.photoUrl)
+                .placeholder(R.drawable.error_logo)
+                .into(binding.ivUser)
+
+        }
+
+        init {
+            binding.rowLY.setOnClickListener {
+                val intent = Intent(context, ProfileActivity::class.java)
+                val carWashModel = list?.get(bindingAdapterPosition)
+                intent.putExtra(Constants.KEY_TYPE, MemberModel.TYPE_SERVICE_PROVIDER)
+                intent.putExtra(Constants.key_provider_data, carWashModel)
+                intent.putExtra(Constants.key_show_profile, true)
+                context.startActivity(intent)
+            }
+
+            binding.detailsBut.setOnClickListener {
+                val intent = Intent(context, ProfileActivity::class.java)
+                val carWashModel = list?.get(bindingAdapterPosition)
+                intent.putExtra(Constants.KEY_TYPE, MemberModel.TYPE_SERVICE_PROVIDER)
+                intent.putExtra(Constants.key_provider_data, carWashModel)
+                intent.putExtra(Constants.key_show_profile, true)
+                context.startActivity(intent)
+            }
+
+        }
+    }
+
+    }
